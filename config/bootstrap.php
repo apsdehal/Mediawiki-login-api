d20ed9f482dd38e41cdd0f3634356949836b3a65
Amanpreet Singh <apsdehal@gmail.com>
2014-05-24 01:22:30 +0530

    Adds sample config file

---
+++ config/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+ require("vendor/autoload.php");
+ 
+ $config = json_decode(file_get_contents("config/config.json", true);
+ 
+ if ($config["environment"] === "development") {
+    error_reporting("-1");
+    ini_set("display_errors", "On");
+ }
+
