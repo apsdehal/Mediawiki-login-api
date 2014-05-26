a41137ef21193dbbb3039cef2efd0505d233f596
Amanpreet Singh <apsdehal@gmail.com>
2014-05-26 15:18:26 +0530

    Comments bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,8 +1,11 @@
 <?php
+ //Load the autoload class provided by the composer
  require("vendor/autoload.php");
- 
+
+ //Get the config and json decode it to get an array with the required config
  $config  = json_decode(file_get_contents("config/config.json"), true);
 
+ 	//Check for the development environment if yes set error display on
 	if ($config["environment"] === "development") {
 	    error_reporting("-1");
 	    ini_set("display_errors", "On");
