0e7a96e5bd8fe6dda9c7f768e2d66e00b1a78de1
Amanpreet Singh <apsdehal@gmail.com>
2014-06-08 23:29:07 +0530

    Cleans HomeController

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -19,73 +19,7 @@
  	 * Also return the user info, <= todo after testing
  	 */	
  	public function get(){
- 		global $config;
-		session_start();
 
- 		// Step 1 - Get a request token
- 		if(!isset($_GET["oauth_verifier"])){
-			list( $redir, $requestToken ) = $this->client->initiate();
-			$_SESSION['requestToken'] = $requestToken;
-
-			// Step 2 - Have the user authorize your app. Get a verifier code from them.
-			// (if this was a webapp, you would redirect your user to $redir, then use the 'oauth_verifier'
-			// GET parameter when the user is redirected back to the callback url you registered.
-			header( "Location:" . $redir);
-		}
-		$verifyCode = $_GET['oauth_verifier'];
-		// $requestToken = $_GET['oauth_token'];
-		// Step 3 - Exchange the request token and verification code for an access token
-		$accessToken = $this->client->complete( $_SESSION['requestToken'],  $verifyCode );
-
-		// // You're done! You can now identify the user, and/or call the API (examples below) with $accessToken
-
-
-		// // If we want to authenticate the user
-		// $identity = $this->client->identify( $accessToken );
-		// echo "Authenticated user {$identity->username}\n";
-
-		// Do a simple API call
-		echo "Getting user info: ";
-		echo $this->client->makeOAuthCall(
-			$accessToken,
-			$config['wiki_url'] . 'api.php?action=query&meta=userinfo&uiprop=rights&format=json'
-		);
-		// var_dump($_GET);
  	}
-
- 	/**
- 	 * Contructer of the HomeController sets up its instance vairable by using libb MWOAuthCLient by Stype
- 	 * and the original OAuth Client, so basically constructes the class.
- 	 */	
- 	public function __construct(){
- 	
- 	// private $mwClient;	global $config;
- 		/* 
- 		   Configure the connection to the wiki you want to use. Passing title=Special:OAuth as a
-		   GET parameter makes the signature easier. Otherwise you need to call
-		   $this->client->setExtraParam('title','Special:OAuth/whatever') for each step.
-		   If your wiki uses wgSecureLogin, the canonicalServerUrl will point to http://
- 	// 	*/
- 	// 	$this->mwConfig = new MWOAuthClientConfig(
-		// 	$config['wiki_url'] . 'index.php?title=Special:OAuth', // url to use
-		// 	true, // do we use SSL? (we should probably detect that from the url)
-		// 	false // do we validate the SSL certificate? Always use 'true' in production.
-		// );
-
-		// $this->mwConfig->canonicalServerUrl = $config['canonical_server'];
-
-		// $this->cmrToken = new OAuthToken( $config['consumer_token'], $config['secret_token'] );
-
-		// $this->client = new MWOAuthClient( $this->mwConfig, $this->cmrToken );
- 	}
- 	function setupSession() {
-		// Setup the session cookie
-		session_name( $this->tool );
-		$params = session_get_cookie_params();
-		session_set_cookie_params(
-			$params['lifetime'],
-			dirname( $_SERVER['SCRIPT_NAME'] )
-		);
-	}
  
  }
