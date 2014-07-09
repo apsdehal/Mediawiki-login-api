d4212e08c90b791d96573c36c907a087905e4736
Amanpreet Singh <apsdehal@gmail.com>
2014-07-09 10:51:07 +0530

    Adds switch cases and basic modes cut and copy paste

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -26,23 +26,111 @@
 
  		$this->mwClient = new MW_OAuth(	'Wikidata-Annotation', 'wikidata', 'www' );
 
- 		switch ( isset( $_GET['action'] ) ? $_GET['action'] : '' ) {
-			case 'download':
-				header( 'Content-Type: text/plain' );
-				readfile( __FILE__ );
-				return;
+ 		$this->checkRedirect();
 
+ 		$this->addHeaders();
+
+		$this->out = array ( 'error' => 'OK' , 'data' => array() );
+
+		$botmode = isset ( $_REQUEST['botmode'] ) ;
+
+		if ( $botmode ) {
+			header ( 'application/json' ) ; // text/plain
+		} else {
+			error_reporting(E_ERROR|E_CORE_ERROR|E_ALL|E_COMPILE_ERROR);
+			ini_set('display_errors', 'On');
+		}
+
+ 		
+		switch ( isset( $_GET['action'] ) ? $_GET['action'] : '' ) {
 			case 'authorize':
-				$this->mwClient->doAuthorizationRedirect();
+				$oa->doAuthorizationRedirect();
+				exit ( 0 ) ;
 				return;
-			case 'check_claim':
-				$params = $this->analyzeGetRequests();
-				echo json_encode($this->mwClient->doesClaimExist($params));
-			case 'check_oauth':
-				echo json_encode($this->mwClient->isAuthOk());
+			
+			case 'remove_claim' :
+				removeClaim() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
+
+			case 'set_claims':
+				setClaims() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
+
+			case 'merge_items':
+				mergeItems() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
+
 			case 'set_label':
-				$params = $this->analyzeGetRequests();
-				echo json_encode($this->mwClient->setLabel($param));			
+				setLabel() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
+
+			case 'set_string':
+				setString() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
+
+			case 'get_rights':
+				getRights() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
+
+			case 'logout':
+				logout() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
+			
+			case 'set_date':
+				setDateClaim() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
+
+			case 'create_item_from_page':
+				createItemFromPage() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
+
+			case 'delete':
+				deletePage() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
+
+			case 'add_row': // Adds a text row to a non-item page
+				addRow() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
+				
+			case 'append' :
+				appendText() ;
+				if ( $botmode ) bot_out() ;
+				else print get_common_footer() ;
+				exit ( 0 ) ;
+				return ;
 		}
  	}
 
@@ -51,6 +139,29 @@
  			unset($_GET['action']);
  		}
  		return $_GET;
+ 	}
+
+ 	public function setRedirect(){
+ 		session_start();
+ 		$_SESSION['redirect_to'] = $_SERVER['HTTP_REFERER'];
+ 		session_write_close();
+ 	}
+
+ 	public function checkRedirect(){
+ 		session_start();
+ 		if(isset($_SESSION['redirect_to']) && $_SESSION['redirect_to']){
+ 			$redirect = $_SESSION['redirect_to'];
+  			unset($_SESSION['redirect_to']);
+  			var_dump($_SESSION);
+ 			header('Location:' . $redirect);
+ 		}
+ 	}
+
+ 	public function addHeaders(){
+		header('Content-type: application/json');
+		header('Access-Control-Allow-Origin: *'); 
+		header('Access-Control-Allow-Headers:Content-Type, X-Requested-With, Accept');
+		header('Access-Control-Allow-Methods:HEAD, GET, POST, PUT, DELETE, OPTIONS');
  	}
  
  }
